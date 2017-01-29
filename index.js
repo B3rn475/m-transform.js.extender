1553e9015935bcc5cb704a90b0d677fc82af1e7d
Carlo Bernaschina <b3rn4@hotmail.it>
2017-01-29 23:44:35 +0100

    Added first version

---
+++ index.js
@@ -0,0 +1,14 @@
+// Copyright (c) 2016, the M-Transform.js project authors. Please see the
+// AUTHORS file for details. All rights reserved. Use of this source code is
+// governed by a MIT-style license that can be found in the LICENSE file.
+/*jslint node: true*/
+"use strict";
+
+var Extender = require('./lib/extender');
+
+// Constructors
+exports.Extender = Extender;
+exports.Rule = Rule;
+
+// Maker Functions
+exports.extend = Extender;
